0433ffcb38a4ff0d6e50a38900758a4568d5c2b4
baharmontazeri <79926642+baharmontazeri@users.noreply.github.com>
2021-03-08 00:24:57 +0330

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(upVideo)
+admin.site.register(upExercise)
+admin.site.register(TeacherOrStudent)
+admin.site.register(Choice)
+admin.site.register(AnsExercise)
